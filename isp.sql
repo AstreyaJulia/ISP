c0f8b0ac6137a2f9df5c4a44647cd86050edabc1
Roman Chernov <76053917+ChernovRoman@users.noreply.github.com>
2021-03-13 16:42:56 +0300

    БД

--- isp.sql
+++ isp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Фев 03 2021 г., 22:51
+-- Время создания: Мар 13 2021 г., 14:06
 -- Версия сервера: 10.3.27-MariaDB-0+deb10u1
 -- Версия PHP: 7.4.10
 
@@ -34,12 +34,12 @@ CREATE TABLE `sdc_calendar` (
   `start` datetime DEFAULT NULL,
   `end` datetime DEFAULT NULL,
   `allDay` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
-  `properties` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `freq` varchar(8) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `properties` text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `freq` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `tzid` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Europe/Moscow',
   `count` tinyint(4) DEFAULT NULL,
   `interval` tinyint(4) DEFAULT NULL,
-  `byweekday` varchar(13) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `byweekday` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `bymonth` varchar(2) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `duration` time DEFAULT NULL,
   `color` varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -52,17 +52,24 @@ CREATE TABLE `sdc_calendar` (
 --
 
 INSERT INTO `sdc_calendar` (`id`, `title`, `description`, `start`, `end`, `allDay`, `properties`, `freq`, `tzid`, `count`, `interval`, `byweekday`, `bymonth`, `duration`, `color`, `url`, `user_id`) VALUES
-(2, 'Справка к совещанию судей', '', '2020-03-06 09:30:00', '2020-03-06 10:00:00', 0, NULL, '', 'Europe/Moscow', NULL, NULL, NULL, NULL, NULL, '#B22222', '', 0),
-(3, 'Закрытие суда', 'Чернов Роман Александрович', '2020-06-01 18:00:00', '2020-06-08 08:30:00', 0, NULL, '', 'Europe/Moscow', NULL, NULL, NULL, NULL, NULL, '#FFD700', '', 0),
-(4, 'Консультант +', 'Отправить статистику Консультант+', '2020-05-20 12:00:00', '2020-05-20 12:30:00', 0, NULL, 'WEEKLY', 'Europe/Moscow', 33, 1, '3', NULL, '02:15:00', '#29ab87', 'sobyitiya/konsultant', 0),
-(5, 'Справка к совещанию председателей', 'Подготовить справку отправить на Смоленский областной суд; \'gk_smoloblsud@mail.ru\'; \'uk_smoloblsud@mail.ru\'; \'adm_smoloblsud@mail.ru\'; \'ok_smoloblsud@mail.ru\'; \'smoloblsud@mail.ru \'', '2020-06-16 12:09:00', NULL, 0, NULL, 'DAILY', 'Europe/Moscow', 1, NULL, NULL, NULL, '00:30:00', '#B22222', '', 0),
-(6, 'Статистическая отчетность', 'До 10 июля', '2020-07-09 12:09:00', NULL, 0, NULL, 'YEARLY', 'Europe/Moscow', 3, NULL, NULL, NULL, '01:30:00', '#B22222', 'sobyitiya/polugodovaya-otchetnost', 0),
-(7, 'Справка', 'Отправить подготовленные спраки', '2020-07-21 12:10:00', NULL, 0, NULL, 'MONTHLY', 'Europe/Moscow', 2, NULL, NULL, NULL, '00:30:00', '#B22222', 'sobyitiya/spravka-k-soveshhaniyu-predsedatelej', 0),
-(8, 'Видеоконференц-связь', 'Проведение тестового сеанса ВКС перед совещанием председателей в 14:00', '2020-07-09 13:30:00', NULL, 0, NULL, 'DAILY', 'Europe/Moscow', 1, NULL, NULL, NULL, '00:30:00', '#c4bd97', '', 0),
-(9, 'Видеоконференц-связь', 'Проведение тестового сеанса ВКС перед совещанием председателей в 14:00', '2020-07-16 13:30:00', NULL, 0, NULL, 'DAILY', 'Europe/Moscow', 1, NULL, NULL, NULL, '02:30:00', '#c4bd97', '', 0),
-(10, 'Видеоконференц-связь', 'Проведение тестового сеанса ВКС перед совещанием по СМИ в 14:00', '2020-07-27 13:30:00', NULL, 0, NULL, 'DAILY', 'Europe/Moscow', 1, NULL, NULL, NULL, '02:30:00', '#c4bd97', '', 0),
-(11, 'test title', 'test description', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, NULL, NULL, 'Europe/Moscow', NULL, NULL, NULL, NULL, NULL, NULL, '', 1),
-(12, 'test title', 'test description', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, NULL, NULL, 'Europe/Moscow', NULL, NULL, NULL, NULL, NULL, NULL, '', 1);
+(2, 'Справка к совещанию судей', '', '2020-03-06 09:30:00', '2020-03-06 10:00:00', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, '#B22222', '', 0),
+(3, 'Закрытие суда', 'Чернов Роман Александрович', '2020-06-01 18:00:00', '2020-06-08 08:30:00', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, '#FFD700', '', 0),
+(4, 'Консультант +', 'Отправить статистику Консультант+', '2020-05-20 12:00:00', '2020-05-20 12:30:00', 0, '', 'WEEKLY', 'Europe/Moscow', 33, 1, '3', NULL, '02:15:00', '#29ab87', 'sobyitiya/konsultant', 0),
+(5, 'Справка к совещанию председателей', 'Подготовить справку отправить на Смоленский областной суд; \'gk_smoloblsud@mail.ru\'; \'uk_smoloblsud@mail.ru\'; \'adm_smoloblsud@mail.ru\'; \'ok_smoloblsud@mail.ru\'; \'smoloblsud@mail.ru \'', '2020-06-16 12:09:00', NULL, 0, '', 'DAILY', 'Europe/Moscow', 1, NULL, '', NULL, '00:30:00', '#B22222', '', 0),
+(6, 'Статистическая отчетность', 'До 10 июля', '2020-07-09 12:09:00', NULL, 0, '', 'YEARLY', 'Europe/Moscow', 3, NULL, '', NULL, '01:30:00', '#B22222', 'sobyitiya/polugodovaya-otchetnost', 0),
+(7, 'Справка', 'Отправить подготовленные спраки', '2020-07-21 12:10:00', NULL, 0, '', 'MONTHLY', 'Europe/Moscow', 2, NULL, '', NULL, '00:30:00', '#B22222', 'sobyitiya/spravka-k-soveshhaniyu-predsedatelej', 0),
+(8, 'Видеоконференц-связь', 'Проведение тестового сеанса ВКС перед совещанием председателей в 14:00', '2020-07-09 13:30:00', NULL, 0, '', 'DAILY', 'Europe/Moscow', 1, NULL, '', NULL, '00:30:00', '#c4bd97', '', 0),
+(9, 'Видеоконференц-связь', 'Проведение тестового сеанса ВКС перед совещанием председателей в 14:00', '2020-07-16 13:30:00', NULL, 0, '', 'DAILY', 'Europe/Moscow', 1, NULL, '', NULL, '02:30:00', '#c4bd97', '', 0),
+(10, 'Видеоконференц-связь', 'Проведение тестового сеанса ВКС перед совещанием по СМИ в 14:00', '2020-07-27 13:30:00', NULL, 0, '', 'DAILY', 'Europe/Moscow', 1, NULL, '', NULL, '02:30:00', '#c4bd97', '', 0),
+(11, 'test title', 'test description', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 1),
+(18, '2', 'test description', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2),
+(19, 'test title', 'test description', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2),
+(20, 'test title', '', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2),
+(21, 'test title', '', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2),
+(22, 'test title', '', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2),
+(23, 'test title', '', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2),
+(24, 'test title', '', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2),
+(25, 'test title', '', '2021-02-02 22:37:02', '2021-02-03 22:37:02', 0, '', '', 'Europe/Moscow', NULL, NULL, '', NULL, NULL, NULL, '', 2);
 
 -- --------------------------------------------------------
 
@@ -102,7 +109,7 @@ INSERT INTO `sdc_pages` (`id`, `title`, `url`, `text`) VALUES
 
 CREATE TABLE `sdc_proxy_list` (
   `id` int(11) UNSIGNED NOT NULL,
-  `menuindex` int(10) DEFAULT NULL,
+  `menuindex` varchar(10) NOT NULL,
   `name_href` varchar(200) NOT NULL,
   `id_group` int(10) NOT NULL DEFAULT 0,
   `href` varchar(200) NOT NULL,
@@ -115,52 +122,52 @@ CREATE TABLE `sdc_proxy_list` (
 --
 
 INSERT INTO `sdc_proxy_list` (`id`, `menuindex`, `name_href`, `id_group`, `href`, `proxy_href`, `created_by`) VALUES
-(1, 0, 'Сервисы', 0, '', '', 1),
-(2, 0, 'Судебная система, госслужба, адвокаты, нотариусы', 0, '', 'sudrf.ru;*.sudrf.ru;arbitr.ru;*.arbitr.ru;*msudrf.ru;*.ssrf.ru;mos-gorsud.ru;', 1),
-(3, 0, 'Органы государственной власти', 0, '', 'gov.ru;*.gov.ru;', 1),
-(4, 0, 'Библиотека', 0, '', '', 1),
-(5, 0, 'Разное', 0, '', '', 1),
-(6, 0, 'blacklist', 0, '', '', 1),
-(7, NULL, 'Почта - кабинет отправителя', 1, 'https://otpravka.pochta.ru/', '*.pochta.ru;*googleapis.com;*recaptcha.net;*gstatic.com;ocsp.pki.goog;91.215.37.248;*.1e100.net;googletagmanager.com;*google.com;', 1),
-(8, NULL, 'Отслеживание почтовых отправлений', 1, 'https://www.pochta.ru/tracking', '', 1),
-(9, NULL, 'Журнал ВКС', 1, 'https://vks.sudrf.ru', '', 1),
-(10, NULL, 'Индексы и телефонные коды', 1, 'http://ruspostindex.ru/', 'ruspostindex.ru;', 1),
-(11, NULL, 'Справочник ВКС', 1, 'http://10.10.1.100/', '10.10.1.100;', 1),
-(12, NULL, 'Заявка на назначение адвоката', 1, 'https://sysadvpcmol.fparf.ru/', 'sysadvpcmol.fparf.ru;smolensk.majc.ru;', 1),
-(13, NULL, 'Получение сведений из ЕГРЮЛ / ЕГРИП', 1, 'https://egrul.nalog.ru/', '', 1),
-(14, NULL, 'Российский союз автостраховщиков', 1, 'https://rsa.org.ru/', 'rsa.org.ru;*.autoins.ru;', 1),
-(15, NULL, 'Универсальный сервис проверки ограничения доступа к сайтам Роскомнадзор', 1, 'https://blocklist.rkn.gov.ru/', '', 1),
-(16, NULL, 'Смоленский областной суд', 2, 'http://oblsud.sml.sudrf.ru/', '', 1),
-(17, NULL, 'Второй кассационный суд', 2, 'https://2kas.sudrf.ru/', '', 1),
-(18, NULL, 'Сафоновский районный суд', 2, 'http://safonovo.sml.sudrf.ru/', '', 1),
-(19, NULL, 'Суды общей юрисдикции г. Москва', 2, 'https://www.mos-gorsud.ru/', 'www.mos-gorsud.ru;', 1),
-(20, NULL, 'МВД', 3, 'https://мвд.рф', 'xn--b1aew.xn--p1ai;*..xn--b1aew.xn--p1ai;мвд.рф;', 1),
-(21, NULL, 'Судебный департамент', 2, 'http://www.cdep.ru/', 'www.cdep.ru;releases.flowplayer.org;cdep.ru;', 1),
-(22, NULL, 'Портал госслужбы', 2, 'https://gossluzhba.gov.ru/', '', 1),
-(23, NULL, 'ГАС \"Правосудие\"', 2, 'https://sudrf.ru/', '', 1),
-(24, NULL, 'Адвокаты Российской Федерации', 2, 'http://lawyers.minjust.ru/Lawyers', '', 1),
-(25, NULL, 'Федеральная нотариальная палата', 2, 'https://notariat.ru/ru-ru/', '*notariat.ru;', 1),
-(26, NULL, 'Муниципальное бразование ', 3, 'http://safonovo-admin.ru/', 'safonovo-admin.ru;', 1),
-(27, NULL, 'Президент России', 3, 'http://kremlin.ru/', '', 1),
-(28, NULL, 'Роскомнадзор', 3, 'https://rkn.gov.ru/', '', 1),
-(29, NULL, 'Государственная дума', 3, 'http://duma.gov.ru/', '', 1),
-(30, NULL, 'Смоленская областная дума', 3, 'http://smoloblduma.ru/', 'smoloblduma.ru;', 1),
-(31, NULL, 'Федеральная служба Судебных приставов', 3, 'http://fssprus.ru/', 'fssprus.ru;*.fssprus.ru;', 1),
-(32, NULL, 'Федеральная налоговая служба', 3, 'https://www.nalog.ru/', '*.nalog.ru;nalog.ru;', 1),
-(33, NULL, 'Росреестр', 3, 'https://rosreestr.ru/', 'rosreestr.ru;', 1),
-(34, NULL, 'ФСИН России', 3, 'http://fsin.su/', 'fsin.su;', 1),
-(35, NULL, 'Министерство Юстиции РФ', 3, 'https://minjust.ru/', '*.minjust.ru;', 1),
-(36, NULL, 'Федеральное казначейство', 3, 'http://roskazna.ru/', 'roskazna.ru;', 1),
-(37, NULL, 'Право.ru', 4, 'https://pravo.ru/', '*.pravo.ru;pravo.ru;', 1),
-(38, NULL, 'Российская газета', 4, 'https://rg.ru/', 'rg.ru;', 1),
-(39, NULL, 'Судебные и нормативные акты РФ (sudact.ru)', 4, 'https://sudact.ru/', 'sudact.ru;', 1),
-(40, NULL, 'Собрание законодательства Российской Федерации', 4, 'http://www.szrf.ru/', 'www.szrf.ru;', 1),
-(41, NULL, 'Федеральный портал проектов нормативных правовых актов', 4, 'https://regulation.gov.ru/', '', 1),
-(42, NULL, 'Изменения законодательства на сайте Президента России', 4, 'http://kremlin.ru/acts/news', '', 1),
-(43, NULL, 'Госуслуги', 5, 'https://www.gosuslugi.ru/', 'gosuslugi.ru;*.gosuslugi.ru;gu-st.ru;', 1),
-(44, NULL, 'Сбер Банк', 5, 'https://www.sberbank.ru/', '', 1),
-(45, NULL, 'Департамент Смоленской области по социальному развитию', 3, 'https://socrazvitie67.ru/', 'socrazvitie67.ru;', 1),
-(46, NULL, 'Консультант+', 4, 'http://www.consultant.ru/', 'consultant.ru;*.consultant.ru;glavkniga.ru;', 1);
+(1, '', 'Сервисы', 0, '', '', 1),
+(2, '', 'Судебная система, госслужба, адвокаты, нотариусы', 0, '', 'sudrf.ru;*.sudrf.ru;arbitr.ru;*.arbitr.ru;*msudrf.ru;*.ssrf.ru;mos-gorsud.ru;', 1),
+(3, '', 'Органы государственной власти', 0, '', 'gov.ru;*.gov.ru;', 1),
+(4, '', 'Библиотека', 0, '', '', 1),
+(5, '', 'Разное', 0, '', '', 1),
+(6, '', 'blacklist', 0, '', '', 1),
+(7, '', 'Почта - кабинет отправителя', 1, 'https://otpravka.pochta.ru/', '*.pochta.ru;*googleapis.com;*recaptcha.net;*gstatic.com;ocsp.pki.goog;91.215.37.248;*.1e100.net;googletagmanager.com;*google.com;', 1),
+(8, '', 'Отслеживание почтовых отправлений', 1, 'https://www.pochta.ru/tracking', '', 1),
+(9, '', 'Журнал ВКС', 1, 'https://vks.sudrf.ru', '', 1),
+(10, '', 'Индексы и телефонные коды', 1, 'http://ruspostindex.ru/', 'ruspostindex.ru;', 1),
+(11, '', 'Справочник ВКС', 1, 'http://10.10.1.100/', '10.10.1.100;', 1),
+(12, '', 'Заявка на назначение адвоката', 1, 'https://sysadvpcmol.fparf.ru/', 'sysadvpcmol.fparf.ru;smolensk.majc.ru;', 1),
+(13, '', 'Получение сведений из ЕГРЮЛ / ЕГРИП', 1, 'https://egrul.nalog.ru/', '', 1),
+(14, '', 'Российский союз автостраховщиков', 1, 'https://rsa.org.ru/', 'rsa.org.ru;*.autoins.ru;', 1),
+(15, '', 'Универсальный сервис проверки ограничения доступа к сайтам Роскомнадзор', 1, 'https://blocklist.rkn.gov.ru/', '', 1),
+(16, '', 'Смоленский областной суд', 2, 'http://oblsud.sml.sudrf.ru/', '', 1),
+(17, '', 'Второй кассационный суд', 2, 'https://2kas.sudrf.ru/', '', 1),
+(18, '', 'Сафоновский районный суд', 2, 'http://safonovo.sml.sudrf.ru/', '', 1),
+(19, '', 'Суды общей юрисдикции г. Москва', 2, 'https://www.mos-gorsud.ru/', 'www.mos-gorsud.ru;', 1),
+(20, '', 'МВД', 3, 'https://мвд.рф', 'xn--b1aew.xn--p1ai;*..xn--b1aew.xn--p1ai;мвд.рф;', 1),
+(21, '', 'Судебный департамент', 2, 'http://www.cdep.ru/', 'www.cdep.ru;releases.flowplayer.org;cdep.ru;', 1),
+(22, '', 'Портал госслужбы', 2, 'https://gossluzhba.gov.ru/', '', 1),
+(23, '', 'ГАС \"Правосудие\"', 2, 'https://sudrf.ru/', '', 1),
+(24, '', 'Адвокаты Российской Федерации', 2, 'http://lawyers.minjust.ru/Lawyers', '', 1),
+(25, '', 'Федеральная нотариальная палата', 2, 'https://notariat.ru/ru-ru/', '*notariat.ru;', 1),
+(26, '', 'Муниципальное бразование ', 3, 'http://safonovo-admin.ru/', 'safonovo-admin.ru;', 1),
+(27, '', 'Президент России', 3, 'http://kremlin.ru/', '', 1),
+(28, '', 'Роскомнадзор', 3, 'https://rkn.gov.ru/', '', 1),
+(29, '', 'Государственная дума', 3, 'http://duma.gov.ru/', '', 1),
+(30, '', 'Смоленская областная дума', 3, 'http://smoloblduma.ru/', 'smoloblduma.ru;', 1),
+(31, '', 'Федеральная служба Судебных приставов', 3, 'http://fssprus.ru/', 'fssprus.ru;*.fssprus.ru;', 1),
+(32, '', 'Федеральная налоговая служба', 3, 'https://www.nalog.ru/', '*.nalog.ru;nalog.ru;', 1),
+(33, '', 'Росреестр', 3, 'https://rosreestr.ru/', 'rosreestr.ru;', 1),
+(34, '', 'ФСИН России', 3, 'http://fsin.su/', 'fsin.su;', 1),
+(35, '', 'Министерство Юстиции РФ', 3, 'https://minjust.ru/', '*.minjust.ru;', 1),
+(36, '', 'Федеральное казначейство', 3, 'http://roskazna.ru/', 'roskazna.ru;', 1),
+(37, '', 'Право.ru', 4, 'https://pravo.ru/', '*.pravo.ru;pravo.ru;', 1),
+(38, '', 'Российская газета', 4, 'https://rg.ru/', 'rg.ru;', 1),
+(39, '', 'Судебные и нормативные акты РФ (sudact.ru)', 4, 'https://sudact.ru/', 'sudact.ru;', 1),
+(40, '', 'Собрание законодательства Российской Федерации', 4, 'http://www.szrf.ru/', 'www.szrf.ru;', 1),
+(41, '', 'Федеральный портал проектов нормативных правовых актов', 4, 'https://regulation.gov.ru/', '', 1),
+(42, '', 'Изменения законодательства на сайте Президента России', 4, 'http://kremlin.ru/acts/news', 'kremlin.ru;', 1),
+(43, '', 'Госуслуги', 5, 'https://www.gosuslugi.ru/', 'gosuslugi.ru;*.gosuslugi.ru;gu-st.ru;', 1),
+(44, '', 'Сбер Банк', 5, 'https://www.sberbank.ru/', '', 1),
+(45, '', 'Департамент Смоленской области по социальному развитию', 3, 'https://socrazvitie67.ru/', 'socrazvitie67.ru;', 1),
+(46, '', 'Консультант+', 4, 'http://www.consultant.ru/', 'consultant.ru;*.consultant.ru;glavkniga.ru;', 1);
 
 -- --------------------------------------------------------
 
@@ -182,9 +189,9 @@ CREATE TABLE `sdc_users` (
 --
 
 INSERT INTO `sdc_users` (`id`, `username`, `password`, `active`, `primary_group`, `sudo`) VALUES
-(1, 'chainik', '$2y$10$L/OG4zk9X.X6JNaataSp5u/aaeENOTUrcofec1dRvbmT6JQPftnLm', 1, 1, 1),
+(1, 'chainik', '$2y$10$QhoOnhyTFuG5M5jTHfySbuY8YyYTsj8FWlJrskOnzttDkL/TEzWVe', 1, 1, 1),
 (2, 'Chernov_RA', 'qwer', 1, 0, 0),
-(3, 'Testin_TT', '$2y$10$mcb9gaI1VxbIRVQ9.IEsxugA2EITW8oxURZnofHHs4pxnjy64nMVa', 1, 0, 0),
+(3, 'Testin_TT', '$2y$10$fkjwkz/Xb8fKVDyHcfaWw.A8AufLz3Ihy/wce06VCsXN8pP6XRis2', 1, 0, 0),
 (4, 'Agibalova_TV', '$2y$10$lCuZeK7ehlrEuP3WlIWTyu0wF5lHvuG.g2alh74GGVe.rE25PHHN6', 1, 10, 0),
 (5, 'Alekseeva_NY', '$2y$10$aUefFQA8OGfjErP97/1MxOMciDXoX24pHZA4t8AoaFXMWOiB13Vkm', 1, 11, 0),
 (6, 'Ashurova_DM', '$2y$10$rF4rtgGsUowpAVEsdm.niOErc8HSVe8J7VXvD9/CnI1wh8//rB/v2', 1, 8, 0),
@@ -353,13 +360,19 @@ ALTER TABLE `sdc_user_attributes`
 -- AUTO_INCREMENT для таблицы `sdc_calendar`
 --
 ALTER TABLE `sdc_calendar`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT для таблицы `sdc_pages`
 --
 ALTER TABLE `sdc_pages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT для таблицы `sdc_proxy_list`
+--
+ALTER TABLE `sdc_proxy_list`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
