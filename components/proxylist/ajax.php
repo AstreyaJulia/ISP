600625d36f67addf33185a0e40e9cd0757438e07
Roman Chernov <76053917+ChernovRoman@users.noreply.github.com>
2021-03-16 21:46:18 +0300

    Update ajax.php
    Создание файлов для CCProxy

--- components/proxylist/ajax.php
+++ components/proxylist/ajax.php
@@ -1,6 +1,35 @@
 <?php
 	$title = "Редактировать запись";
 	$desc = "Описание чего-то коротко";
+
+	function WriteProxyList ($link) {
+		//Получаем все name_href
+		$query_name_href = "SELECT id, id_group, proxy_href FROM sdc_proxy_list WHERE name_href IS NOT NULL";
+		$result_name_href = mysqli_query($link, $query_name_href) or die (mysqli_error($link));
+		//получаем массив с записью
+		for ($all_name_href = []; $row = mysqli_fetch_assoc($result_name_href); $all_name_href[] = $row);
+		$id = 6; //Группа blacklist 
+		$id_group = 6; //Ссылки из группы blacklist
+		$output_whitelist = "";
+		$output_blacklist = "";
+		
+		$whitelist = array_filter($all_name_href, function ($value) use ($id, $id_group) {
+			return ($value["id"] != $id and $value["id_group"] != $id_group);
+		}, ARRAY_FILTER_USE_BOTH);
+		foreach ($whitelist as $resourse) {
+			$output_whitelist .= $resourse["proxy_href"];
+		}
+		file_put_contents('components/proxylist/data/whitelist.txt', $output_whitelist);
+
+		$blacklist = array_filter($all_name_href, function ($value) use ($id, $id_group) {
+			return ($value["id"] == $id or $value["id_group"] == $id_group);
+		}, ARRAY_FILTER_USE_BOTH);
+		foreach ($blacklist as $resourse) {
+			$output_blacklist .= $resourse["proxy_href"];
+		}
+		file_put_contents('components/proxylist/data/blacklist.txt', $output_blacklist);
+	}
+
 //Редактируем ссылку
 	if (!empty($_GET["editLink"])) {
 		//Получаем все группы
@@ -31,7 +60,7 @@
 				$query_update = "UPDATE sdc_proxy_list SET menuindex='$menuindex', id_group='$id_group', href='$href', name_href='$name_href', proxy_href='$proxy_href' WHERE id='$id'";
 				mysqli_query($link, $query_update);
 				//переходим в раздел
-				//header( "refresh:1;url=/?page=proxylist" );
+				WriteProxyList ($link);
 				header("Location: /?page=proxylist");
 			}
 		} else {//добавляем ссылку
@@ -50,6 +79,9 @@
 				$proxy_href = $_POST["proxy_href"];
 				$query_insert = "INSERT INTO sdc_proxy_list (menuindex, id_group, href, name_href, proxy_href) VALUES ('$menuindex', '$id_group', '$href', '$name_href', '$proxy_href')";
 				mysqli_query($link, $query_insert);
+				//переписываем файлы для CCProxy
+				WriteProxyList ($link);
+				//переходим в раздел
 				header("Location: /?page=proxylist");
 			}
 		}
@@ -76,8 +108,9 @@
 				$proxy_href = $_POST["proxy_href"];
 				$query_update = "UPDATE sdc_proxy_list SET menuindex='$menuindex', name_href='$name_href', proxy_href='$proxy_href' WHERE id='$id'";
 				mysqli_query($link, $query_update);
+				//переписываем файлы для CCProxy
+				WriteProxyList ($link);
 				//переходим в раздел
-				//header( "refresh:1;url=/?page=proxylist" );
 				header("Location: /?page=proxylist");
 			}
 		} else {//добавляем ссылку
@@ -91,7 +124,9 @@
 				$proxy_href = $_POST["proxy_href"];
 				$query_insert = "INSERT INTO sdc_proxy_list (menuindex, name_href, proxy_href) VALUES ('$menuindex', '$name_href', '$proxy_href')";
 				mysqli_query($link, $query_insert);
-				echo "Добавил группу";
+				//переписываем файлы для CCProxy
+				WriteProxyList ($link);
+				//переходим в раздел
 				header("Location: /?page=proxylist");
 			}
 		}
