19b11f7ee9df97342513dbc28b18d08f0b535974
Юлия Латышева <58434576+AstreyaJulia@users.noreply.github.com>
2021-08-02 10:25:35 +0300

    Обновление bat файлов и конфигов

--- assets_updater.bat
+++ assets_updater.bat
@@ -1,3 +1,6 @@
 @echo 
 XCOPY /Y /H /R dist\assets\css\* assets\css\
 XCOPY /Y /H /R dist\assets\js\* assets\js\
+XCOPY /Y /H /R dist\assets\fonts\* assets\fonts\
+XCOPY /Y /H /R dist\assets\img\* assets\img\
+XCOPY /Y /H /R dist\assets\modules\* assets\modules\
