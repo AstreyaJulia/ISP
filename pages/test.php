eba0f2c185043d678da38757d152ec03082d5427
ChernovRoman <79002251616@ya.ru>
2021-08-16 17:29:48 +0300

    дни рождения для fullcalendar

--- pages/test.php
+++ pages/test.php
@@ -2,7 +2,51 @@
 $title = "title Тестовой страницы";
 $info = "Блок info для тестовой страницы";
 
-ob_start();
+$FullcalendarClass = new \Core\Model\Fullcalendar($db);
+
+
+$startParam = "1957-12-01";
+$data = DateTime::createFromFormat('Y-m-d', $startParam);
+$startParam = $data->format('m');
+
+$endParam = "1957-12-01";
+$data = DateTime::createFromFormat('Y-m-d', $endParam);
+$endParam = $data->format('m');
+
+
+
+
+$params = [$startParam, $endParam];
+
+
+$title = "День рождения";
+$description = "Фамилия И.О. возраст";
+
+$events = $FullcalendarClass->getBirthday($params);
+
+var_dump($events);
+
+
+
+
+
+
+
+
+
+    $content = $data->format('m');
+
+
+
+
+
+
+
+
+
+
+
+/*ob_start();
 include "components/test/template/tpl.test.php";
 $content = ob_get_contents();
-ob_end_clean();
+ob_end_clean();*/
