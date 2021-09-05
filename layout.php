42fa1211a021f4347329d58a263fe0ca54c6db08
Юлия Латышева <58434576+AstreyaJulia@users.noreply.github.com>
2021-09-05 21:25:18 +0300

    Перекрыли виджету погоды все, что было можно

--- layout.php
+++ layout.php
@@ -11,42 +11,42 @@ if (file_exists($path)) {
 ?>
 <body class="page-body">
 <div class="page-wrapper">
-<?php
-//подключаем header
-$path = "elements/header.php";
-if (file_exists($path)) {
-  include "elements/header.php";
-} else {
-  echo "не подключен: elements/header.php";
-}
-?>
-<?php
-//подключаем sidebar
-$path = "elements/sidebar.php";
-if (file_exists($path)) {
-  include "elements/sidebar.php";
-} else {
-  echo "не подключен: elements/sidebar.php";
-}
-?>
-<?php
-//подключаем content
-$path = "elements/content.php";
-if (file_exists($path)) {
-  include "elements/content.php";
-} else {
-  echo "не подключен: elements/content.php";
-}
-?>
-<?php
-//Подключаем javascript
-$path = "elements/javascript.php";
-if (file_exists($path)) {
-  include "elements/javascript.php";
-} else {
-  echo "не подключен: elements/javascript.php";
-}
-?>
+  <?php
+  //подключаем header
+  $path = "elements/header.php";
+  if (file_exists($path)) {
+    include "elements/header.php";
+  } else {
+    echo "не подключен: elements/header.php";
+  }
+  ?>
+  <?php
+  //подключаем sidebar
+  $path = "elements/sidebar.php";
+  if (file_exists($path)) {
+    include "elements/sidebar.php";
+  } else {
+    echo "не подключен: elements/sidebar.php";
+  }
+  ?>
+  <?php
+  //подключаем content
+  $path = "elements/content.php";
+  if (file_exists($path)) {
+    include "elements/content.php";
+  } else {
+    echo "не подключен: elements/content.php";
+  }
+  ?>
+  <?php
+  //Подключаем javascript
+  $path = "elements/javascript.php";
+  if (file_exists($path)) {
+    include "elements/javascript.php";
+  } else {
+    echo "не подключен: elements/javascript.php";
+  }
+  ?>
 </div>
 </body>
 </html>
