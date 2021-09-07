aec30ca4135de541cd81ee6fdd280db0826eb0e9
ChernovRoman <79002251616@ya.ru>
2021-09-07 20:06:11 +0300

    json для fullcalendar

--- pages/test.php
+++ pages/test.php
@@ -4,17 +4,33 @@ $info = "Блок info для тестовой страницы";
 $content = "";
 
 
-$xml = simplexml_load_file('data/weekend/2021.xml');
-
-//$xml = new SimpleXMLElement($str);
-
-$content = $xml->holidays->holiday['title']; 
+/*$xml = simplexml_load_file('data/weekend/2021.xml');
+$content = $xml->holidays->holiday['title'];*/
 
 
 $ourData = file_get_contents("data/weekend/2021.json");
+//$row = json_encode($ourData);
 $row = json_decode($ourData);
 
-var_dump($row);
+//$content .= $row->year;
+foreach ($row->months as $value) {
+	//$content .= "</br>месяц: " .$value->month. "</br>";
+	//$content .= "день: " .$value->days. "</br>";
+	$days = explode(',', $value->days);
+	foreach ($days as $day) {
+		$day = rtrim($day, "*");
+		$content .= $row->year."-".addNol($value->month)."-".addNol($day)."</br>";
+	}
+}
 
 
 
+
+
+function addNol($str) { 
+	if (strlen($str) == 1) {
+		return '0'.$str;
+	} else {
+		return $str;
+	}
+}
