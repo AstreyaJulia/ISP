e1fe2881cd306d980a722554086834aff14f1fc4
Юлия Латышева <astreyajulia@gmail.com>
2021-11-05 23:30:52 +0300

    CSS рефакторинг

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -128,7 +128,7 @@ function getCookie(name) {
   const ca = document.cookie.split(';');
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
     if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
   }
   return null;
@@ -677,7 +677,7 @@ const minicalendarhandler = () => {
       const colorName = calendarsColor[calendarEvent._def.extendedProps.calendar];
       return [
         // Фоновый цвет событий
-        'bg-' + colorName + '-50'
+        'fc-event-' + colorName
       ];
     },
     headerToolbar: {
@@ -1570,7 +1570,7 @@ const calendmodulehandler = () => {
       const colorName = calendarsColor[calendarEvent._def.extendedProps.calendar];
       return [
         // Фоновый цвет событий
-        'bg-' + colorName + '-50'
+        'fc-event-' + colorName
       ];
     },
     eventSources: [fetchEvents],
