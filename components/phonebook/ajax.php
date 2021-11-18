ef7a4202da3808be19d9023c71a4410ded0ae2f8
ChernovRoman <79002251616@ya.ru>
2021-11-18 23:24:07 +0300

    FIX: для запроса в формате http://isp/components/phonebook/ajax.php?filter=24,25,26,27

--- components/phonebook/ajax.php
+++ components/phonebook/ajax.php
@@ -1,28 +1,24 @@
 <?php
-// включаем автозагрузку классов
-spl_autoload_register(function($class) {
-	require (mb_strtolower($_SERVER['DOCUMENT_ROOT'] . '/' . str_replace('\\', '/', $class) . '.php'));
-});
-error_reporting(E_ALL);
-ini_set("display_errors", "on");
-//подключаемся к базе
-require_once $_SERVER['DOCUMENT_ROOT'] . "/conection.php";
-//подключаем функции
-require_once $_SERVER["DOCUMENT_ROOT"] . "/core/extension/custom_functions.php";
-//подключаем справочники
-require_once $_SERVER["DOCUMENT_ROOT"] . "/core/extension/reference_book.php";
+	// включаем автозагрузку классов
+	spl_autoload_register(function($class) {
+		require (mb_strtolower($_SERVER['DOCUMENT_ROOT'] . '/' . str_replace('\\', '/', $class) . '.php'));
+	});
+	error_reporting(E_ALL);
+	ini_set("display_errors", "on");
+	//подключаемся к базе
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/conection.php";
+	//подключаем функции
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/core/extension/custom_functions.php";
+	//подключаем справочники
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/core/extension/reference_book.php";
 
-//Подключаемся  базе
-$db = new \Core\Config\DB($dbname, $user, $password, $host);
+	//Подключаемся  базе
+	$db = new \Core\Config\DB($dbname, $user, $password, $host);
 
-
-// собираем массив для запроса
-if (isset($_GET["filter"])) {
-  foreach ($_GET["filter"] as $key => $value) {
-    if ($_GET["filter"] [$key] != 0) {
-      $primary_group[] = $_GET["filter"] [$key] . ",";
-    }
-  }
+	// собираем массив для запроса
+	if (isset($_GET["filter"])) {
+	  $primary_group = explode(',', $_GET["filter"]);
+	}
 
 	$phonebookClass = new \Core\Model\Phonebook($db);
 	$phonebook = $phonebookClass->getSelect($primary_group);
@@ -35,4 +31,3 @@ if (isset($_GET["filter"])) {
 		<td>' . $row->phone_worck . '</td>
 	</tr>' . PHP_EOL;
 	}
-}
