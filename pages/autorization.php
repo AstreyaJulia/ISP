8f843b39841bac01f5f022c7c43f9ea15d82081f
ChernovRoman <79002251616@ya.ru>
2021-11-29 17:41:51 +0300

    fix: вернул ошибки при авторизации

--- pages/autorization.php
+++ pages/autorization.php
@@ -2,14 +2,12 @@
 
   $autorizationClass = new \Core\Model\Autorization($db);
 
-  // авторизация пользователя
-  $error = $autorizationClass->setUserAutorization() ?? array ("pass" => "", "login" => "");
-
-  //Регистрация пользователя
-  $error = $autorizationClass->setUserRregister() ?? array ("pass" => "", "login" => "");
-
   if (array_key_exists('reg', $_GET)) {
+    //Регистрация пользователя
+    $error = $autorizationClass->setUserRregister() ?? array ("pass" => "", "login" => "");
     include $_SERVER['DOCUMENT_ROOT'] . "/components/autorization/tpl.register.php";
   } else {
+    // авторизация пользователя
+    $error = $autorizationClass->setUserAutorization() ?? array ("pass" => "", "login" => "");
     include $_SERVER['DOCUMENT_ROOT'] . "/components/autorization/tpl.autorization.php";
   }
