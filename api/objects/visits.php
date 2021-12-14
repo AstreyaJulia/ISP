fa8f5fc6bfb6957ad879d29b309dd54d8c8223cb
ChernovRoman <79002251616@ya.ru>
2021-12-14 23:06:04 +0300

    add: Статистика посещений портала

---
+++ api/objects/visits.php
@@ -0,0 +1,56 @@
+<?php
+	namespace Api\Objects;
+	use Core\Config\DB;
+
+	class Visits {
+
+		protected $db;
+
+	    public function __construct(DB $db) {
+	        $this->db = $db;
+	    }
+	    
+	    public function visits() {
+	    	/*
+для графиков нужен формат json:
+[
+  {
+    "count": [21, 215, 5, 115, 1],
+    "day": ["2021-10-20", "2021-10-22", "2021-10-27", "2021-10-28", "2021-10-30"],
+  }
+]
+	    	*/
+
+	        $sql = "SELECT
+	        			count(id) AS count,
+					    DATE_ADD((CAST(dtime AS DATE)), INTERVAL 1 DAY) AS day
+					FROM
+					    sdc_visits
+					GROUP BY
+					    day
+					ORDER BY
+					    day";
+	        return $this->db->run($sql)->fetchAll(\PDO::FETCH_CLASS);
+	    }
+
+	    // Групируем статистику посещений
+	    	    	/*
+			для графиков нужен формат json:
+			[
+			  {
+			    "count": [21, 215, 5, 115, 1],
+			    "day": ["2021-10-20", "2021-10-22", "2021-10-27", "2021-10-28", "2021-10-30"],
+			  }
+			]
+	    	*/
+	    public function getVisits() {
+	        foreach ($this->visits() as $key => $value) {
+	        	$data["count"][] = $value->count;
+	        	$data["day"][] = $value->day;
+	        }
+	        $data["count"] = array(implode(",", $data["count"]));
+	        $data["day"] = array(implode(",", $data["day"]));
+	        return $data;
+	    }
+
+	}
