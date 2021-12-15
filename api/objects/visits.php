398e7b6792cadf6a98ba6e2d49c7e29f5bb072dd
ChernovRoman <79002251616@ya.ru>
2021-12-15 17:56:04 +0300

    fix: правки для json

--- api/objects/visits.php
+++ api/objects/visits.php
@@ -11,25 +11,15 @@
 	    }
 	    
 	    public function visits() {
-	    	/*
-для графиков нужен формат json:
-[
-  {
-    "count": [21, 215, 5, 115, 1],
-    "day": ["2021-10-20", "2021-10-22", "2021-10-27", "2021-10-28", "2021-10-30"],
-  }
-]
-	    	*/
-
 	        $sql = "SELECT
 	        			count(id) AS count,
-					    DATE_ADD((CAST(dtime AS DATE)), INTERVAL 1 DAY) AS day
+					    date_format(DATE_ADD((CAST(dtime AS DATE)), INTERVAL 1 DAY),'%d-%m-%Y') AS day
 					FROM
 					    sdc_visits
 					GROUP BY
 					    day
 					ORDER BY
-					    day";
+					    dtime";
 	        return $this->db->run($sql)->fetchAll(\PDO::FETCH_CLASS);
 	    }
 
@@ -48,8 +38,6 @@
 	        	$data["count"][] = $value->count;
 	        	$data["day"][] = $value->day;
 	        }
-	        $data["count"] = array(implode(",", $data["count"]));
-	        $data["day"] = array(implode(",", $data["day"]));
 	        return $data;
 	    }
 
