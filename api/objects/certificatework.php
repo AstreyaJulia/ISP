102cb7a0344b0947bd0cb9a516e61bccf7042d91
Julia Latysheva <astreyajulia@gmail.com>
2021-12-22 17:03:37 +0300

    UPD: Фамилии судей

--- api/objects/certificatework.php
+++ api/objects/certificatework.php
@@ -12,11 +12,14 @@
 	    
 	    public function getSelect($quarter, $year) {
 	    	$prepare = str_repeat('?,', count($quarter) - 1) . '?';
-	        $sql = "SELECT 
-						SUM(3) AS `3`,
-					    SUM(4) AS `4`,
-					    SUM(5) AS `5`
-					FROM sdc_certificate_work 
+	        $sql = "SELECT
+	        			UserAttributes.fullname,
+						SUM(`3`) AS `col_3`,
+					    SUM(`4`) AS `col_4`,
+					    SUM(`5`) AS `col_5`
+					FROM sdc_certificate_work AS CertWork
+						LEFT JOIN `sdc_users` AS Users ON CertWork.judges = Users.id
+						LEFT JOIN `sdc_user_attributes` AS UserAttributes ON Users.id = UserAttributes.internalKey
 					WHERE quarter IN($prepare) AND year = ?
 					GROUP BY judges ORDER BY judges";
 					$quarter[] = $year;
