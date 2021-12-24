c6a8070998f619e99f918f67dc2ab32e54c616d4
Юлия Латышева <astreyajulia@gmail.com>
2021-12-24 18:24:59 +0300

    FIX: путь к api сайдбара

--- elements/sidebar.php
+++ elements/sidebar.php
@@ -3,7 +3,6 @@ $sudo = $_COOKIE['aut']['sudo'];
 $path = $host_api. $_SERVER['SERVER_NAME']. "/api/elements/sidebar.php?sudo=$sudo";
 $ourData = file_get_contents($path);
 $menu = json_decode($ourData);
-var_dump($path);
 ?>
 <!-- Сайдбар -->
 <aside class="main-sidebar d-flex position-absolute flex-column">
