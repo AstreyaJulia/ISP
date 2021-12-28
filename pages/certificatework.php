2fbbdee5b9dc99894ccd6460512bee3d9e536ec3
ChernovRoman <79002251616@ya.ru>
2021-12-28 17:38:40 +0300

    add: Редактирование справки

--- pages/certificatework.php
+++ pages/certificatework.php
@@ -4,7 +4,16 @@ $ourData = file_get_contents($path);
 $row = json_decode($ourData);
 $title = "Справка о работе судей";
 
+$verification = array_key_exists('edit', $_GET) ?? "";
 ob_start();
-    include "components/certificatework/tpl.certificatework.php";
+switch ($verification) {
+    case 'true':
+        include "components/certificatework/tpl.edit.certificatework.php";
+        break;
+    
+    default:
+        include "components/certificatework/tpl.certificatework.php";
+        break;
+}
     $content = ob_get_contents();
 ob_end_clean();
