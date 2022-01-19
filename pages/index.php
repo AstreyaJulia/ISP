11d162b3692ebdf3c40def23767aa509a75dbae8
ChernovRoman <79002251616@ya.ru>
2022-01-19 19:44:31 +0300

    fix: заглушка еслли недоступен ГАС

--- pages/index.php
+++ pages/index.php
@@ -4,9 +4,16 @@ $title = "Главная страница";
 $UserAttributes = new \Core\Model\UserAttributes($db);
 $birthday = $UserAttributes->getBirthday();
 
-$path = 'http://192.168.0.254:8079/api_GAS/oracle.php?idJudge=8500013';
+/*$path = 'http://192.168.0.254:8079/api_GAS/oracle.php?idJudge=8500013';
 $ourData = file_get_contents($path);
-$row = json_decode($ourData);
+$row = json_decode($ourData);*/
+$row = [
+	"FULL_NUMBER" => "",
+	"VERDICT_DATE"  => "",
+	"VALIDITY_DATE"  => "",
+	"DATE_UNTILL"  => "",
+	"STAT"  => ""
+];
 $notPub = count($row);
 
 ob_start();
