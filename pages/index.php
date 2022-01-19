fbc106a6714378f1b83a25852a89d248ea1521cf
ChernovRoman <79002251616@ya.ru>
2022-01-19 23:54:35 +0300

    fix: заглушка если недоступен ГАС

--- pages/index.php
+++ pages/index.php
@@ -4,21 +4,27 @@ $title = "Главная страница";
 $UserAttributes = new \Core\Model\UserAttributes($db);
 $birthday = $UserAttributes->getBirthday();
 
-if (file_exists("http://192.168.0.254:8079/api_GAS/oracle.php?idJudge=8500013")) {
-	$ourData = file_get_contents("http://192.168.0.254:8079/api_GAS/oracle.php?idJudge=8500013");
-	$row = json_decode($ourData);
+
+$url_file = "http://192.168.0.254:8079/api_GAS/oracle.php?idJudge=8500013";
+$file_headers = @get_headers($url_file);
+
+if ($file_headers) {
+    $ourData = file_get_contents("http://192.168.0.254:8079/api_GAS/oracle.php?idJudge=8500013");
+    $row = json_decode($ourData);
+    $notPub = count($row);
 } else {
-	$row = array();
-	$value = new class {
+    $row = array();
+    $value = new class {
         public $FULL_NUMBER = false;
         public $VERDICT_DATE = false;
         public $VALIDITY_DATE = false;
         public $DATE_UNTILL = false;
         public $STAT = false;
     };
+    $notPub = "Что-то пошло не так";
 }
 // количество неопубликованых актов
-$notPub = count($row);
+
 ob_start();
 include "components/index/template/tpl.index.php";
 $content = ob_get_contents();
