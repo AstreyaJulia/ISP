c0c5449af1063188d56171ba603ffd7fee7a4c46
Roman Chernov <79002251616@ya.ru>
2022-06-02 17:51:07 +0300

    upd: функционал на будущее

--- api/v1/routers/users.php
+++ api/v1/routers/users.php
@@ -14,18 +14,39 @@ function route($db, $helpers) {
         // установим код ответа - 200 OK
         http_response_code(200);
         // вывод в json-формате
-        $helpers->getJsonEncode("Полный список пользователей в разработке");
+        $helpers->getJsonEncode(array("data" => array("info" => "Полный список пользователей в разработке")));
         break;
       }
       // GET /users/parameter
       case 2: {
         // если запрос с параметрами отдаём запрашиваемую запись
-        $userLoginData["data"] = [
-          "id" => $helpers->getId(),
-          "sudo" => $helpers->getSudo(),
-          "username" => $helpers->getUsername(),
-          "fullname" => $helpers->getFullname()];
-        $helpers->getJsonEncode($userLoginData);
+        switch ($helpers->getUrlData()[1]) {
+          case "login-data": {
+            $userLoginData["data"] = [
+              "id" => $helpers->getId(),
+              "sudo" => $helpers->getSudo(),
+              "username" => $helpers->getUsername(),
+              "fullname" => $helpers->getFullname()];
+            $helpers->getJsonEncode($userLoginData);
+            break;
+          }
+          case "birthday": {
+            $helpers->getJsonEncode(array("data" => array("info" => "дни рождения в разработке")));
+            break;
+          }
+          case "asistant": {
+            $helpers->getJsonEncode(array("data" => array("info" => "Список помощников в разработке")));
+            break;
+          }
+          case "secretary": {
+            $helpers->getJsonEncode(array("data" => array("info" => "Список секретарей с.з. в разработке")));
+            break;
+          }
+          default:
+          // если переданы лишние параметры выбрасываем ошибку
+          $helpers::isErrorInfo(400, "invalid_router", "router not found");
+          break;
+        }
         break;
       }
       default:
