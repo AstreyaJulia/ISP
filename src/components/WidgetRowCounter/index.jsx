0bca05aa72b145fb127f8c21229ca98fc821bbd7
Julia Latysheva <astreyajulia@gmail.com>
2022-10-20 21:30:59 +0300

    upd: test

--- src/components/WidgetRowCounter/index.jsx
+++ src/components/WidgetRowCounter/index.jsx
@@ -5,8 +5,8 @@ import { AvatarCircularWithPlaceholderInitials } from "../AvatarCircularWithPlac
 import { getAmount } from "../../utils/getAmount";
 
 const WidgetRowCounter = ({ rows, isLoading, error, title, color, link, counter: { ...counter } }) => {
+
   return (
-    (
       error === null ?
         <div
           className="rounded-lg relative group bg-white dark:bg-gray-900 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 shadow">
@@ -44,8 +44,23 @@ const WidgetRowCounter = ({ rows, isLoading, error, title, color, link, counter:
             </div>
           </Link>
         </div>
-        : ""
-    )
+        : error === 'ГАС недоступен' ? <div
+          className="rounded-lg relative group bg-white dark:bg-gray-900 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 shadow">
+            <div className="flex items-center p-3">
+              <>
+                <AvatarCircularWithPlaceholderInitials name={''} size="14"
+                                                       color={color}
+                                                       icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                                       </svg>
+                                                       }
+                                                       shape="roundedLG" classname="flex-shrink-0" />
+                <div className="min-w-0 flex flex-col ml-4 text-sm">
+                  <span className="line-clamp-3">{error}</span>
+                </div>
+              </>
+            </div>
+        </div> : ''
   );
 };
 
