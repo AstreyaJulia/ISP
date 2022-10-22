2b4792c6e66db14a9f1ee90fbf83b436423d5a38
Julia Latysheva <astreyajulia@gmail.com>
2022-10-22 16:17:13 +0300

    upd: test

--- src/components/DataTable/Processed/index.jsx
+++ src/components/DataTable/Processed/index.jsx
@@ -12,14 +12,14 @@ const Processed = ({ data, isLoading, all }) => {
 
   const [rows, setRows] = useState(data ?? []);
   const columns = Object.keys(data[0] ?? []);
-  const [selectedFilter, setSelectedFilter] = useState({ JUDGE: "All", CASE_STATUS: "All" });
+  const [selectedFilter, setSelectedFilter] = useState({ JUDGE_NAME: "All", CASE_STATUS: "All" });
   const [currentPage, setCurrentPage] = useState(0); // текущая страница
   const [judgesList, setJudgesList] = useState([]);
   const [statusList, setStatusList] = useState([]);
 
   useEffect(() => {
     setRows(data);
-    setJudgesList(getUniqueArrayValuesByKey(data ?? [], "JUDGE").sort((a, b) => a.localeCompare(b)));
+    setJudgesList(getUniqueArrayValuesByKey(data ?? [], "JUDGE_NAME").sort((a, b) => a.localeCompare(b)));
     setStatusList(getUniqueArrayValuesByKey(data ?? [], "CASE_STATUS").sort((a, b) => a.localeCompare(b)));
     // eslint-disable-next-line
   }, [isLoading]);
@@ -43,10 +43,10 @@ const Processed = ({ data, isLoading, all }) => {
                       color={caseTypesSettings[item?.CASE_TYPE].color} classname="mr-2" />
               <span>{getHighlightedText(item?.CASE_NUMBER, query)}</span>
             </p>
-            <span className="text-xs text-indigo-700 dark:text-indigo-300 mb-2">{getInitials(item?.JUDGE)}</span>
+            <span className="text-xs text-indigo-700 dark:text-indigo-300 mb-2">{getInitials(item?.JUDGE_NAME)}</span>
             <p
               className="text-sm text-slate-700 dark:text-slate-300 flex flex-wrap line-clamp-1 justify-start items-center text-left">
-              {getHighlightedText(item?.PARTS_NAMES, query)}
+              {getHighlightedText(item?.PARTS_FIO, query)}
             </p>
           </div>
           <div className="flex flex-col items-end shrink-0 gap-1 h-full grow-0">
@@ -63,7 +63,7 @@ const Processed = ({ data, isLoading, all }) => {
     </div>;
 
   const filter = (rows, query, columns) => rows?.filter((row) =>
-    columns.slice(3, 7).filter((item) => item !== "JUDGE" && item !== "JUDGE_ID").some(
+    columns.slice(3, 7).filter((item) => item !== "JUDGE_NAME" && item !== "JUDGE_ID").some(
       (column) => row[column].toLowerCase().indexOf(query.toLowerCase()) > -1
     )
   );
@@ -115,14 +115,14 @@ const Processed = ({ data, isLoading, all }) => {
     >
       <div className="p-3 bg-slate-100 rounded-md mt-3 mx-4 flex items-center gap-2">
         {all === "true" ? <div className="flex items-center ml-3 justify-start">
-          <label htmlFor="JUDGE"
+          <label htmlFor="JUDGE_NAME"
                  className="shrink-0 block text-sm font-medium text-slate-700 dark:text-slate-300 mr-2">
             Судья:
           </label>
           <select
-            id="JUDGE"
-            name="JUDGE"
-            defaultValue={selectedFilter.JUDGE}
+            id="JUDGE_NAME"
+            name="JUDGE_NAME"
+            defaultValue={selectedFilter.JUDGE_NAME}
             onChange={filterSelectChangeHandler}
             className="grow-0 mt-1 block pl-3 pr-10 py-2 text-base bg-white dark:bg-slate-900 border-slate-300 dark:border-slate-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
           >
