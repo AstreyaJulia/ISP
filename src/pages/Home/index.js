07734c0e71e7a189a8c53192ba9b0910082247d2
Julia Latysheva <astreyajulia@gmail.com>
2022-10-23 14:47:29 +0300

    upd: test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,15 +5,16 @@ import useAuth from "../../hooks/useAuth";
 import NoPublicatedActs from "./widgets/NoPublicatedActs";
 import ProcessedWidget from "./widgets/ProcessedWidget";
 import UserWelcomeWidget from "./widgets/UserWelcomeWidget";
+import { classNames } from "../../utils/classNames";
 
 const Home = () => {
 
   /** Состояние пользователя */
-  const { user } = useAuth();
+  const { user, sidebar } = useAuth();
 
   return (
     <BasicPage title="Главная" className="main-content mx-auto px-5">
-      <div className="pt-2 flex sm:grid md:grid-cols-2 2xl:grid-cols-3 gap-3 sm:gap-5 xl:gap-7">
+      <div className={classNames(sidebar?.toString() === "0" ? 'xl:grid-cols-3' : 'xl:grid-cols-2', "pt-2 flex sm:grid md:grid-cols-2 gap-3 sm:gap-5 xl:gap-7")}>
         <div>
           <div className="flex flex-col gap-4">
             {/* 1-я колонка */}
