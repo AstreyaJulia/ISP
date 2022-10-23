9bd27d71a2d0fd9f73148abd769fa6ddbd729428
Julia Latysheva <astreyajulia@gmail.com>
2022-10-23 11:31:09 +0300

    upd: test

--- src/components/DataTable/PublicationControl/index.jsx
+++ src/components/DataTable/PublicationControl/index.jsx
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from "react";
 import { compareDesc, intervalToDuration, parse } from "date-fns";
 import ru from "date-fns/locale/ru";
 import PropTypes from "prop-types";
-import { Avatar } from "../../Avatar";
-import { caseTypesSettings } from "../../../config";
 import DataTableCore from "../DataTableCore";
 import Badge from "../../Badge";
 import { getInitials } from "../../../utils/getInitials";
-import { getHighlightedText } from "../../../utils/getHighlightedText";
 import { getUniqueArrayValuesByKey } from "../../../utils/getArrayValuesByKey";
+import CaseListCard from "../../CaseListCard";
 
 const PublicationControl = ({ data, isLoading, all }) => {
 
@@ -45,41 +43,21 @@ const PublicationControl = ({ data, isLoading, all }) => {
     date: PropTypes.string.isRequired
   };
 
-  const makeItem = (item, key, query) => <div key={key}
-                                              className="p-3 w-full flex items-center rounded-md bg-slate-100 dark:bg-slate-800">
-    <div className="flex grow items-center">
-
-      <div className="flex items-center justify-between w-full">
-        <div className="flex flex-col items-start">
-          <p
-            className="font-medium text-sm text-slate-800 dark:text-slate-200 flex flex-wrap line-clamp-1 justify-start items-center text-left mb-1">
-            <Avatar size="6" shape="circle" name={item?.CASE_TYPE}
-                    color={caseTypesSettings[item?.CASE_TYPE].color} classname="mr-2" />
-            <span>{getHighlightedText(item?.CASE_NUMBER, query)}</span>
-
-          </p>
-          <span className="text-xs text-indigo-700 dark:text-indigo-300 mb-2">{getInitials(item?.JUDGE_NAME)}</span>
-          <p
-            className="text-sm text-slate-700 dark:text-slate-300 flex flex-wrap line-clamp-1 justify-start items-center text-left">{getHighlightedText(item?.PARTS_FIO, query)}</p>
-        </div>
-        <div className="flex flex-col items-end shrink-0 gap-1">
-          {item?.DATE_UNTILL !== null ? <p
-            className="font-medium text-xs text-slate-800 dark:text-slate-200 flex flex-wrap line-clamp-1 justify-start items-center text-left mb-1">До:
-            <Badge size="small" shape="rounded" className="ml-1"
-                   color={getCaseUntilColor(item?.DATE_UNTILL ?? null)}
-                   item={item?.DATE_UNTILL !== null ? `${item?.DATE_UNTILL}` : ""} />
-          </p> : <Badge size="small" shape="rounded"
-                        color="indigo"
-                        item="Не вступило" />}
-          <p
-            className="font-medium text-xs text-slate-600 dark:text-slate-200 flex flex-wrap justify-start items-center text-left mb-1">Рассм.: {item?.VERDICT_DATE}</p>
-          <p
-            className="font-medium text-xs text-slate-600 dark:text-slate-200 flex flex-wrap justify-start items-center text-left mb-1">{item?.PUBLICATION_STATUS}</p>
-
-        </div>
-      </div>
-    </div>
-  </div>;
+  const makeItem = (item, key, query) =>
+    <CaseListCard key={key} item={item} query={query}>
+      {item?.DATE_UNTILL !== null ? <p
+        className="font-medium text-xs text-slate-800 dark:text-slate-200 flex flex-wrap line-clamp-1 justify-start items-center text-left mb-1">До:
+        <Badge size="small" shape="rounded" className="ml-1"
+               color={getCaseUntilColor(item?.DATE_UNTILL ?? null)}
+               item={item?.DATE_UNTILL !== null ? `${item?.DATE_UNTILL}` : ""} />
+      </p> : <Badge size="small" shape="rounded"
+                    color="indigo"
+                    item="Не вступило" />}
+      <p
+        className="font-medium text-xs text-slate-600 dark:text-slate-200 flex flex-wrap justify-start items-center text-left mb-1">Рассм.: {item?.VERDICT_DATE}</p>
+      <p
+        className="font-medium text-xs text-slate-600 dark:text-slate-200 flex flex-wrap justify-start items-center text-left mb-1">{item?.PUBLICATION_STATUS}</p>
+    </CaseListCard>;
 
   const filter = (rows, query, columns) => rows?.filter((row) =>
     columns.slice(2, 6).filter((item) => item !== "JUDGE_NAME" && item !== "JUDGE_ID").some(
