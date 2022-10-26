e442f51598d3fb79e04b9f5f6630c317b456a1b8
Roman Chernov <79002251616@ya.ru>
2022-10-26 17:44:44 +0300

    add: новый класс Users

---
+++ api/v1/objects/Users.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Api\Objects;
+
+class Users
+{
+
+  protected Helpers $helpers;
+
+  public function __construct(
+    Helpers $helpers = new \Api\Objects\Helpers()
+  ) {
+    $this->helpers = $helpers;
+  }
+
+  /**
+   * Список работающих сотрудников
+   */
+  public function userList(): array
+  {
+    $sql = "SELECT
+                    Users.id,
+                    UserAttributes.fullname,
+                    ParentUserType.name AS room,
+                    Vocation.name AS profession,
+                    ChildUserType.phone_worck
+              FROM `sdc_room` AS ChildUserType
+                      LEFT JOIN `sdc_room` AS ParentUserType ON ChildUserType.affiliation = ParentUserType.id
+                      LEFT JOIN `sdc_user_attributes` AS UserAttributes on ChildUserType.id = UserAttributes.room
+                      LEFT JOIN `sdc_users` AS Users ON UserAttributes.internalKey=Users.id
+                      LEFT JOIN `sdc_vocation` AS Vocation ON Vocation.id=UserAttributes.profession
+              WHERE Users.active = 1 and UserAttributes.profession != ''";
+    return $row = $this->helpers->db->run($sql)->fetchAll(\PDO::FETCH_ASSOC);
+  }
+
+  /**
+   * Обрабатываем приходящие GET-запросы. 
+   */
+  private function metodGET()
+  {
+    switch (count($this->helpers->getUrlData())) {
+        // GET /users
+      case 1: {
+          // если запрос без параметров отдаём полный список
+
+          // установим код ответа - 200 OK
+          http_response_code(200);
+          // вывод в json-формате
+          $this->helpers::getJsonEncode($this->helpers::wrap($this->userList(), "data"));
+          break;
+        }
+        // GET /users/parameter
+      case 2: {
+          // если запрос с параметрами отдаём запрашиваемую запись
+          switch ($this->helpers->getUrlData()[1]) {
+            case "login-data": {
+                $userLoginData["data"] = [
+                  "id" => $this->helpers->id,
+                  "sudo" => $this->helpers->sudo,
+                  "username" => $this->helpers->username,
+                  "fullname" => $this->helpers->fullname,
+                  "professionID" => $this->helpers->professionID,
+                  "professionName" => $this->helpers->professionName,
+                  "sidebar" => $this->helpers->sidebar,
+                  "theme" => $this->helpers->theme
+                ];
+                $this->helpers->getJsonEncode($userLoginData);
+                break;
+              }
+            case "birthday": {
+                $output["data"] = !empty($this->helpers->getBirthday()) ? $this->helpers->getBirthday() : $this->helpers::isErrorInfo(200, "Нет данных", "Сегодня дней рождений нет");
+                $this->helpers->getJsonEncode($output);
+                break;
+              }
+            case "asistant": {
+                $this->helpers::isErrorInfo(200, "Нету", "Список помощников в разработке");
+                break;
+              }
+            case "secretary": {
+                $this->helpers::isErrorInfo(200, "Нету", "Список секретарей с.з. в разработке");
+                break;
+              }
+            default:
+              // если переданы лишние параметры выбрасываем ошибку
+              $this->helpers::isErrorInfo(400, "invalid_router", "router not found");
+              break;
+          }
+          break;
+        }
+      default:
+        // если переданы лишние параметры выбрасываем ошибку
+        $this->helpers::isErrorInfo(400, "invalid_router", "router not found");
+        break;
+    }
+  }
+
+  /**
+   * Обрабатываем приходящие POST-запросы. 
+   */
+  private function metodPOST()
+  {
+    $this->helpers::headlinesPOST();
+
+    switch ($this->helpers->getUrlData()[1] ?? false) {
+      case 'login-data': {
+
+          $param = $this->helpers->getFormData() ? implode(array_keys($this->helpers->getFormData())) : false;
+
+          if (in_array($param, ["sidebar", "theme"])) {
+            try {
+              $output["data"] = $this->helpers->setUserSettings($param, (int)$this->helpers->getFormData()[$param]);
+              $this->helpers->getJsonEncode($output);
+            } catch (\PDOException $e) {
+              $this->helpers::isErrorInfo(200, "Што-то пошло не так", $e);
+            }
+          } else {
+            $this->helpers::isErrorInfo(400, "invalid_router", "router not found");
+          }
+          break;
+        }
+      default:
+        // если переданы лишние параметры выбрасываем ошибку
+        $this->helpers::isErrorInfo(400, "invalid_router", "router not found");
+        break;
+    }
+  }
+
+  public function responseUsers()
+  {
+    match ($this->helpers->getMethod()) {
+      "GET" => $this->metodGET(),
+      "POST" => $this->metodPOST()
+    };
+  }
+}
