c2cf92c7ab3556e6d81b9daed19cab305af0386c
Julia Latysheva <astreyajulia@gmail.com>
2022-11-02 23:17:34 +0300

    upd: test

--- src/pages/Test/test4.js
+++ src/pages/Test/test4.js
@@ -27,7 +27,7 @@ const Faq = () => {
           <>
             <Disclosure.Button
               className={classNames(
-                'pl-2 w-full flex items-center rounded-md shrink-0 w-full my-1',
+                'pl-2 py-1 w-full flex items-center rounded-md shrink-0 w-full my-1',
                 open
                   ? 'text-indigo-800 dark:text-indigo-200'
                   : 'text-slate-800 dark:text-slate-200'
@@ -35,16 +35,17 @@ const Faq = () => {
             >
               <div className="flex grow items-center">
                 <div className="flex flex-col items-start">
-                  <p className="text-sm flex justify-start text-left mb-1">
-                    {open ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-gray-400">
+                  <p className="text-sm flex justify-start text-left mb-1 gap-2">
+                    {open ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-gray-400 mt-1">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                      :
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-gray-400">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-gray-400 mt-1">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
                     }
-                    <img src={icons[item.icon]} alt='Значок' className='h-4 w-4 mx-1 mt-1'/>
+                    <span className='border-t border-gray-400 w-1 h-1 mt-3'/>
+                    <img src={icons[item.icon]} alt='Значок' className='h-4 w-4 mt-1'/>
                     <span className="flex flex-wrap items-center">{item.title}</span>
                   </p>
                 </div>
@@ -55,9 +56,9 @@ const Faq = () => {
               {item.children?.length > 0 ? (
                 item.children.map((item, key) => sidebarLink(item, key, count))
               ) : (
-                <p className="text-sm pl-2 w-full flex rounded-md shrink-0 w-full">
-                  <img src={icons[item.icon]} alt='Значок' className='h-4 w-4 mx-1 mt-1'/>
-                  {item.title}
+                <p className="text-sm pl-2 w-full flex rounded-md shrink-0 w-full gap-2">
+                  <img src={icons[item.icon]} alt='Значок' className='h-4 w-4 mt-1'/>
+                  <span>{item.title}</span>
                 </p>
               )}
             </Disclosure.Panel>
@@ -68,9 +69,9 @@ const Faq = () => {
       <p
         title={item.subtitle !== '' ? `${item.title} ${item.subtitle}` : item.title}
         key={key}
-        className="text-sm flex justify-start text-left mb-1 text-slate-800 dark:text-slate-200"
+        className="text-sm py-1 flex justify-start text-left mb-1 text-slate-800 dark:text-slate-200 gap-2"
       >
-        <img src={icons[item.icon]} alt='Значок' className='h-4 w-4 mx-1 mt-1'/>
+        <img src={icons[item.icon]} alt='Значок' className='h-4 w-4 mt-1'/>
         <span>{item.title} {item.subtitle}</span>
       </p>
     );
