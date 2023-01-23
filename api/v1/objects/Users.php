e80dbbc8572492d8c5b976689e92cb79966274a0
ChernovRoman <79002251616@ya.ru>
2023-01-23 20:26:25 +0300

    upd: Список сотрудников по группам

--- api/v1/objects/Users.php
+++ api/v1/objects/Users.php
@@ -67,14 +67,21 @@ class Users
    */
   public function usersGroup()
   {
-    $param = $this->helpers->urlData[1];
+    
+    $param = $this->helpers->urlData[2];
+
+    if ( filter_var($param, FILTER_VALIDATE_INT) === false ) {
+      $this->helpers::isErrorInfo(400, "Неверные параметры", "Ожидаю целое число. Нолучаю: $param");
+    }
 
     $sql = "SELECT
-                id,
-                name AS label
-            FROM sdc_vocation
-            WHERE parent_id $param";
-    return $this->helpers->db->run($sql)->fetchAll(\PDO::FETCH_CLASS);
+              userAttr.id,
+              userAttr.fullname AS label
+            FROM sdc_user_attributes AS userAttr
+            LEFT JOIN sdc_vocation AS vocation ON vocation.id = userAttr.profession
+            LEFT JOIN sdc_users AS users ON users.id = userAttr.internalKey 
+            WHERE vocation.parent_id = :param AND users.active = 1";
+    return $this->helpers->db->run($sql,[$param])->fetchAll(\PDO::FETCH_CLASS);
   }
 
 
@@ -134,7 +141,7 @@ class Users
           break;
         }
       case 3: {
-        echo "сотрудники по группам";
+        $this->helpers->getJsonEncode($this->usersGroup());
         break;
       }
       default:
