51c2f0649c9ea6a915b782edb81d0b57b1c5836b
Roman Chernov <79002251616@ya.ru>
2023-03-22 16:15:39 +0300

    upd: привел авторизацию к общему виду

--- api/v1/routers/authorization.php
+++ api/v1/routers/authorization.php
@@ -1,33 +1,5 @@
 <?php
 
-  include_once 'config/jwt.php';
+
   $authorizationClass = new Api\Objects\Authorization($helpers);
-  // POST
-    switch ($helpers->method) {
-      // GET /authorization
-      case "POST": {
-        $authorizationClass->routAuthorization();
-        $token = array(
-          "iss" => $iss,
-          "aud" => $aud,
-          "iat" => $iat,
-          "nbf" => $nbf,
-          "data" => array(
-            "id" => $helpers->id,
-            "sudo" => $helpers->sudo,
-            "professionID" => $helpers->professionID
-          )
-        );
-
-        http_response_code(200);
-
-        $jwt["data"] = ["jwt" => \Firebase\JWT\JWT::encode($token, $key)];
-
-        $helpers->getJsonEncode($jwt);
-        break;
-      }
-      default:
-        // если переданы лишние параметры выбрасываем ошибку
-        $helpers::isErrorInfo(400, "invalid_router", "router not found");
-        break;
-    }
+  $authorizationClass->response();
