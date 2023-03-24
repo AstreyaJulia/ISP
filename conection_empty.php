b35c5bb9e20de1df83c58f98e50a6affea88a314
Roman Chernov <79002251616@ya.ru>
2023-03-24 12:23:18 +0300

    upd: Настройка для отображения погоды

--- conection_empty.php
+++ conection_empty.php
@@ -1,11 +1,18 @@
 <?php
 
-  	$host_api = "http://" . $_SERVER['SERVER_NAME'];
-
-	$api_gas = "http://";
-
 	define("API_GAS", "место нахождения папки со скриптами для ГАС");
 	define("DB_HOST", "путь до базы данных");
 	define("DB_NAME", "имя базы данных");
 	define("DB_USER", "пользователь");
 	define("DB_PASS", "пароль");
+
+	/** 
+	 * Настройка для отображения погоды
+	 * Координаты города. 
+	 * Искать кликом на карту https://yandex.ru/maps/ и нажав на название улицы или места,
+	 * слева в меню будут показаны координаты  
+	 * 
+	 */
+
+	define('OPEN_WEATHER_API_CURRENT', 'https://openweathermap.org/data/2.5/onecall?');
+	define('OPEN_WEATHER_API_KEY', '439d4b804bc8187953eb36d2a8c26a02');
