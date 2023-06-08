863673491084b54fb8c9aad5da2c67a9b523ffe5
Roman Chernov <79002251616@ya.ru>
2023-06-08 15:36:02 +0300

    upd: вместо NULL возвращаем ""

--- api/v1/objects/DB.php
+++ api/v1/objects/DB.php
@@ -17,6 +17,7 @@ class DB
       \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
       \PDO::ATTR_EMULATE_PREPARES => false,
       \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+      \PDO::ATTR_ORACLE_NULLS => \PDO::NULL_TO_STRING
     ];
     $options = array_replace($default_options, $options);
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
